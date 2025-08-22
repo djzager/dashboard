576271a473b0e12229745e2083c326e3f9d5b9e4
David Zager <david.j.zager@gmail.com>
2025-08-22 16:54:48 -0400

    :bug: Include pi ip in cors origin
    Signed-off-by: David Zager <david.j.zager@gmail.com>

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -11,7 +11,7 @@ const PORT = process.env.PROXY_PORT || 3001
 
 // Enable CORS for frontend
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://10.1.10.3:3000'],
   credentials: true,
   exposedHeaders: ['link', 'x-total-count', 'x-page', 'x-per-page', 'x-total-pages']
 }))
